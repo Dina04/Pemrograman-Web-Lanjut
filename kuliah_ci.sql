16de2d2a8be410ec54fe5162eeb70eccdef11611
Dina04 <56152656+Dina04@users.noreply.github.com>
2020-03-09 15:48:24 +0700

    Pemrograman Web Lanjut

--- kuliah_ci.sql
+++ kuliah_ci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2020 at 12:45 AM
+-- Generation Time: Mar 09, 2020 at 09:45 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
